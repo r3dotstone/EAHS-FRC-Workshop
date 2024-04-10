09ea18b2d94dcf6d0b749707b7f157a04fbaa76f
Jaden <jstone.import@gmail.com>
2024-04-10 18:02:01 -0400

    pins

--- test/test.ino
+++ test/test.ino
@@ -1,9 +1,18 @@
 #include <ME480FSM.h>
-
+// https://electricdiylab.com/controlling-n20-encoder-micro-gear-motor-with-arduino/
 FSMEncoder1 encode1;
+
+const int speed = 1;
+const int dir = 2;
+long counts = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(counts, INPUT);
+  pinMode(speed, OUTPUT);
+  pinMode(dir, OUTPUT);
 }
 
 void loop() {
